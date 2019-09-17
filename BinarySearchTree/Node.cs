2e6167415891e34e0cf94c73f50a49ca83d7f328
Jason Bowles <jbowles96@gmail.com>
2019-09-17 11:56:36 -0500

    Insert() method initialized | TODO: run a few tests on the insert method

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Node
+    {
+        public int data;
+        public Node rightChild;
+        public Node leftChild;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ BinarySearchTree/SearchTree.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class SearchTree
+    {
+        public Node rootNode;
+        
+
+        public void Insert(int value)
+        {
+            Node node = new Node(value);
+
+            if(rootNode == null)
+            {
+                rootNode = node;
+                return;
+            }
+            else
+            {
+                Node temporaryNode = rootNode;
+                bool valueIsGreater = CompareNodeValues(value);
+                while (valueIsGreater)
+                {
+                    if(temporaryNode.rightChild != null)
+                    {
+                        temporaryNode = temporaryNode.rightChild;
+                    }
+                    temporaryNode.rightChild = node;
+                }
+                if(!valueIsGreater)
+                {
+                    if(temporaryNode.leftChild != null)
+                    {
+                        temporaryNode = temporaryNode.leftChild;
+                    }
+                    temporaryNode.leftChild = node;
+                }
+            }
+        }
+
+        public void Search(int value)
+        {
+
+        }
+
+        private bool CompareNodeValues(int value)
+        {
+            bool greaterValue = false;
+
+            if(value > rootNode.data)
+            {
+                greaterValue = true;
+            }
+            else
+            {
+                greaterValue = false;
+            }
+            return greaterValue;
+        }
+    }
+}
